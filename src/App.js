0254302f57239badb41f51c93bbe46b4c4680b76
unknown <anuchouhan718@gmail.com>
2022-07-11 13:22:19 +0530

    myreactsocial app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import {Person} from "@material-ui/icons";
+import Profile from "./pages/profile/Profile";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Home from './pages/home/Home';
+function App() {
+    return(
+      <>
+      <Home/>
+    {/* <Register/> */}
+    {/* <Login/> */}
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/share/Share.js
@@ -0,0 +1,39 @@
+import "./share.css";
+import { PermMedia ,Label,Room,EmojiEmotions } from "@material-ui/icons";
+
+export default function Share(){
+    return(
+        <>
+            <div className="share">
+                <div className="shareWrapper ">
+                      <div className="shareTop">
+                          <img className="shareProfileImg" src="/assets/person/1.jpeg" alt=""/>
+                          <input placeholder="What's in your mind Aakansksha?" type="text" className="shareInput"/>
+                      </div>
+                      <hr className="shareHr"/>
+                      <div className="shareButton">
+                          <div className="shareOptions">
+                              <div className="shareOption">
+                                  <PermMedia htmlColor="tomato" className="shareIcon"/>
+                                  <span className="shareOptionText">Photo or Video</span>
+                              </div>
+                              <div className="shareOption">
+                                  <Label htmlColor="blue" className="shareIcon"/>
+                                  <span className="shareOptionText">Tag</span>
+                              </div>
+                              <div className="shareOption">
+                                  <Room htmlColor="blue" className="shareIcon"/>
+                                  <span className="shareOptionText">Location</span>
+                              </div>
+                              <div className="shareOption">
+                                  <EmojiEmotions htmlColor="yellow" className="shareIcon"/>
+                                  <span className="shareOptionText">Feelings</span>
+                              </div>
+                              <button className="shareButtons">Share</button>
+                          </div>
+                      </div>
+                </div>
+            </div>
+        </>
+    );
+}
